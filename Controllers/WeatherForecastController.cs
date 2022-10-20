afb6e739a6970822f09be82ffb77e901758ef7ad
Tomasz Rajzer <trajzer@wsiz.edu.pl>
2022-10-20 12:55:41 +0200

    init

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public int Get(int value)
+        {
+            var calculateInstance = new Calculate(value);
+            calculateInstance.Increase(13);
+            return calculateInstance.GetValue();
+        }
+
+    }
+}
